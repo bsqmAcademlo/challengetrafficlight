02e0a4df2b26ef8b52dc996e7b4897aa9441e1f7
bsqmAcademlo <brayan.munoz@academlo.com>
2023-07-13 17:29:28 -0500

    add funcionalities

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ export const App = () => {
 	const [color, setColor] = useState();
 	const [flag, setFlag] = useState(false);
 	const [time, setTime] = useState(1000);
+	const [clock, setClock] = useState(1);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setClock(clock + 1);
+
+			if (clock === time / 1000) {
+				setClock(1);
+			}
+		}, 1000);
+
+		return () => clearTimeout(timeout);
+	}, [clock, time]);
 
 	useEffect(() => {
 		setColor("red");
@@ -29,25 +42,34 @@ export const App = () => {
 
 	return (
 		<>
-			<div className="semaforo">
-				<div className={`item ${color === "red" ? "rojo" : ""}`}></div>
+			<div className="trafficLight">
+				<div className={`item ${color === "red" ? "item-red" : ""}`}>
+					<p>{clock} s</p>
+				</div>
 				<div
-					className={`item ${color === "yellow" ? "amarillo" : ""}`}
-				></div>
+					className={`item ${
+						color === "yellow" ? "item-yellow" : ""
+					}`}
+				>
+					<p>{clock} s</p>
+				</div>
 				<div
-					className={`item ${color === "green" ? "verde" : ""}`}
-				></div>
+					className={`item ${color === "green" ? "item-green" : ""}`}
+				>
+					<p>{clock} s</p>
+				</div>
 			</div>
 			<select
 				onChange={(e) => {
 					setTime(+e.target.value);
+					setClock(1);
 				}}
 				className="select"
 			>
-				<option value="1000">1s</option>
-				<option value="2000">2s</option>
-				<option value="3000">3s</option>
-				<option value="4000">4s</option>
+				<option value="1000">1 segundo</option>
+				<option value="2000">2 segundos</option>
+				<option value="3000">3 segundos</option>
+				<option value="4000">4 segundos</option>
 			</select>
 		</>
 	);
